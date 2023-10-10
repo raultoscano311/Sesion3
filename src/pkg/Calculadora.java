474c619a5a29626c1aa790b7bd3a1fd0d92d61db
usuario <usuario@pc20-020>
2023-10-10 10:17:56 +0200

    tdd paso 1

---
+++ src/pkg/Calculadora.java
@@ -0,0 +1,15 @@
+package pkg;
+
+public class Calculadora {
+
+	public static Integer suma(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
+
+	public static Integer resta(int i, int j) {
+		// TODO Auto-generated method stub
+		return i-j;
+	}
+
+}
